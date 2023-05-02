229c557994e1e782ef5aea433c4359a7712c7646
alain <alain.santamaria57@gmail.com>
2023-05-02 17:32:54 +0200

    transformation du Composant App en class pour MEP un state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,51 @@
+// == Import npm 
+import React from 'react';
+
 // == Import
-import taskList from 'src/data/tasks';
+import tasksList from 'src/data/tasks';
 import Form from '../Form';
 import Counter from '../Counter';
 import Tasks from '../Tasks';
 import './app.scss';
+import { Component } from 'react';
 
 // == Composant
-function App() {
-  return (
-    <div className="app">
-      <Form />
-      <Counter />
-      <Tasks tasks={taskList} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // On veut utilisé des tâches du fichier src/data/tasks.js comme tâche initiales
+      tasks: tasksList,
+    };
+  }
+
+  render() {
+    // On calcul le nombre de tâche non réalisées
+    const notDoneTasks = tasksList.filter((item) => item.done === false);
+    const nbTasksNotDone = notDoneTasks.length;
+
+    return (
+      <div className="app">
+        <Form />
+        <Counter nbTasks={nbTasksNotDone} />
+        <Tasks tasks={tasksList} />
+      </div>
+    );
+  }
 }
+// function App() {
+//   // On calcul le nombre de tâche non réalisées
+//   const notDoneTasks = tasksList.filter((item) => item.done === false);
+//   const nbTasksNotDone = notDoneTasks.length;
+
+//   return (
+//     <div className="app">
+//       <Form />
+//       <Counter nbTasks={nbTasksNotDone} />
+//       <Tasks tasks={tasksList} />
+//     </div>
+//   );
+// }
 
 // == Export
 export default App;
--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,12 +1,17 @@
 // == Import npm
+import PropTypes from 'prop-types';
+// == Import local
 import './counter.scss';
 
 // == Composant
-function Counter() {
+function Counter({ nbTasks }) {
   return (
-    <p className="counter">2 tâches en cours</p>
+    <p className="counter">{nbTasks} tâches en cours</p>
   );
 }
 
+Counter.propTypes = {
+  nbTasks: PropTypes.number.isRequired,
+};
 // == Export
 export default Counter;
